97177967310ae5f420687508095f1a74879f846c
ZJ <zj781119282@163.com>
2019-11-20 13:41:31 +0800

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <SoundSwitcher />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import SoundSwitcher from '@/components/SoundSwitcher'
+
+export default {
+  name: 'App',
+  components: {
+    SoundSwitcher,
+  },
+}
+</script>
+
+<style>
+html {
+  font-size: 15.625vw;
+}
+#app {
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+}
+</style>
---
+++ src/components/SoundSwitcher.vue
@@ -0,0 +1,38 @@
+<template>
+  <i class="iconfont" :class="sound ? 'rotate' : ''" @click="sound = !sound">{{sound ? '&#xe990;' : '&#xe608;'}}</i>
+</template>
+
+<script>
+export default {
+  name: 'SoundSwitcher',
+  data() {
+    return {
+      sound: true,
+    }
+  },
+}
+</script>
+
+<style lang=scss scoped>
+  .iconfont {
+    position: fixed;
+    right: 20px;
+    top: 20px;
+    font-size: 30px;
+    color: rgb(1, 165, 175);
+  }
+  .rotate {
+    animation: rotate 2s infinite linear;
+  }
+  @keyframes rotate {
+    0% {
+      transform: rotate(0);
+    }
+    50% {
+      transform: rotate(180deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
